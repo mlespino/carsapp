fac1cc67978e0c30bf4078cf8dfcc1524fac94f6
mespino <you@example.com>
2020-08-19 12:02:22 -0300

    Pagination

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="d-flex justify-content-between align-item-center pt-3 pb-3 mb-3 border-bottom">
         <h2>Cars</h2>
-        <div class="btn-toolbar mb-2 mb-md-0">
+        <div class="btn-toolbar mb-2 mb-md-3">
             <a href="/cars/create" class="btn btn-sm btn-primary">Add New Car</a>
         </div>
     </div>
@@ -35,19 +35,20 @@
                 </tr>
             @endforeach
         </table>
-            <nav aria-label="Pagination">
-                <ul class="pagination pagination-sm justify-content-center">
+            <nav aria-label="Pagination" class="d-flex justify-content-between align-items-center px-2">
+                <p>Showing {{$cars->firstItem()}} to {{$cars->lastItem()}} of {{$cars->total()}} entries</p>
+                <ul class="pagination pagination-sm">
                     <form class="form-inline mr-1" method="GET" action="" role="form">
                         <div class="form-group">
                             <label for="perPage">Items per page</label>
                             <select name="perPage" id="perPage" class="form-control form-control-sm ml-1" onchange ="this.form.submit()">
-                                <option value="5" @if ($cars->perPage === 5) selected @endif>5</option>
-                                <option value="10" @if ($cars->perPage === 10) selected @endif>10</option>
-                                <option value="15" @if ($cars->perPage === 15) selected @endif>15</option>
+                                <option value="5" @if ($cars->perPage() == 5) selected @endif>5</option>
+                                <option value="10" @if ($cars->perPage() == 10) selected @endif>10</option>
+                                <option value="15" @if ($cars->perPage() == 15) selected @endif>15</option>
                             </select>
                         </div>
                     </form>
-                    {{$cars->links()}}
+                    {{$cars->appends(['perPage' => $cars->perPage()])->links()}}
                 </ul>
             </nav>
         </div>
